276585b380c074ac0dde8c8c09183b10ec3c1bba
Bernardo Kopp <m1t0.faerless@gmail.com>
2021-06-27 13:56:02 -0300

    Modificado programa base

--- src/bench/Bench.java
+++ src/bench/Bench.java
@@ -1,7 +1,10 @@
 package bench;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 class Multithreading extends Thread {
-   
+
     public void run() {
         double var;
         try {
@@ -12,29 +15,44 @@ class Multithreading extends Thread {
             var = Math.sqrt(7.35);
             var = 2.57 / 7.77;
             var = Math.exp(3.95);
+            System.out.println(var);
         } catch (Exception e) {
             System.out.println("Exception is caught");
         }
+
     }
 }
 
 public class Bench {
 
     public static void main(String[] args) {
-        final int NUM_THREADS = Runtime.getRuntime().availableProcessors() + 1;
-        int loops = 30000;
+        final int NUM_THREADS = Runtime.getRuntime().availableProcessors();
+        long tinicial = System.currentTimeMillis();
+        int loops = 10;
         int score = 0;
-        // Esse for era para executar infinitamente eles, até o tempo limite. Mas não pode utilizar time. a
-        for (int y = 0; y < loops; y++) {
-            // Esse loop é para executar elas todas, porém tem que arrumar um meio de executar elas independentes.
-            for (int i = 0; i < NUM_THREADS; i++) {
-                Multithreading object = new Multithreading();
-                object.start();
-                score++;
+
+        Collection<Multithreading> threads = new ArrayList<>();
+
+        for (int i = 1; i <= NUM_THREADS; i++) {
+            Multithreading thread = new Multithreading();
+            thread.setName("Thread " + i);
+            threads.add(thread);
+        }
+
+        for (Multithreading mt : threads) {
+            mt.start();
+        }
+
+        for (Multithreading mt : threads) {
+            try {
+                mt.join();
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
             }
         }
-        System.out.println();
-        System.out.println(score);
+
+        System.out.println("Tempo: " + (System.currentTimeMillis() - tinicial));
+
     }
 
 }
