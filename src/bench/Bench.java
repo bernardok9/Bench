b09b3db1f4331adfe60643358e88c6363325492d
Bernardo Kopp <m1t0.faerless@gmail.com>
2021-06-28 03:09:49 -0300

    Processamento de núcleos em paralelo funcional

--- src/bench/Bench.java
+++ src/bench/Bench.java
@@ -1,45 +1,74 @@
 package bench;
 
-import java.util.ArrayList;
-import java.util.Collection;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class Bench {
+    //Um "Executável" do teste para realizar os experimentos. Não sei se tem necessidade de mexer aqui.
+    private static Runnable runzinho = new Runnable() { 
 
-    public static void main(String[] args) {
-        final int NUM_THREADS = Runtime.getRuntime().availableProcessors();
-        int loops = 10;
-        int score = 0;
-        long tinicial = System.currentTimeMillis();
+        @Override
+        public void run() {
+            double var;
+            try {
 
-        Collection<Thread> threads = new ArrayList<>();
-        for (int j = 0; j < 10000; j++) {
-            //Cria os Thread baseado nos núcleos do pc;
-            for (int i = 1; i <= NUM_THREADS; i++) {
-                Thread thread = new Thread(new Multithreading());
-                thread.setName("Thread " + i);
-                threads.add(thread);
+                var = Math.log10(37.71);
+                var = Math.sin(2.46);
+                var = Math.cos(0.27);
+                var = Math.sqrt(7.35);
+                var = 2.57 / 7.77;
+                var = Math.exp(3.95);
+            } catch (Exception e) {
+                System.out.println("error teste");
             }
-
-            //Inicia os Thread;
-            for (Thread th : threads) {
-                th.start();
-
-            }
-
-            //Starta threads já criados;
-            for (Thread th : threads) {
-                try {
-                    th.join();
-                } catch (InterruptedException ex) {
-                    ex.printStackTrace();
-                }
-            }
-
-            threads.clear();
         }
-        
-        System.out.println("Tempo: " + (System.currentTimeMillis() - tinicial));
+    };
 
+    public static void main(String[] args) throws Exception {
+        //Função para rodar o teste.
+        benchmark();
+        
     }
 
+    private static void benchmark() throws InterruptedException {
+        //Ele dá get na quantidade de núcleos do seu pc
+        final int NUM_CORES = Runtime.getRuntime().availableProcessors();
+        //Score para o benchmark
+        int score = 0;
+        //ExecutorService "Biblioteca de Threads" ;; "newWorkStealingPool" função para paralelismo com o nível desejado.
+        ExecutorService executorService = Executors.newWorkStealingPool(NUM_CORES);
+        //Get no horário do sistema inicial.
+        long tinicial = System.nanoTime();
+        //Função para rodar o teste
+        for (int i = 0; i < 20000000; i++) {
+            //Executa várias vezes o Runnable desejado, definindo as thread automaticamente baseado na função acima.
+            executorService.submit(runzinho);
+            score++;
+        }
+        //Encerra o executorService.
+        executorService.shutdown();
+        //Espera 30seg para finalizar as thread pendentes.
+        executorService.awaitTermination(30, TimeUnit.SECONDS);
+        //Get no horário do sistema final.
+        long tfinal = System.nanoTime();
+        long tempomedio = 0;
+        //Calcula o tempo médio e converte para milissegundos.
+        tempomedio += ((tfinal - tinicial)/1000000);
+        
+        //Reseta o garbage colletor do pc. Para não retornar sempre "pré-definido" o tempo médio de execução.
+        System.gc();
+
+        //Print dos resultados da run.
+        System.out.println("Nucleos usados: " + NUM_CORES + "\nTempo: " + (float)tempomedio/1000 + " Segundos\n" + "Score: " + score);
+    }
 }
+
+/*
+Referências: 
+https://www.baeldung.com/java-executor-service-tutorial
+https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/util/concurrent/Executors.html#newWorkStealingPool()
+https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/lang/Runtime.html#availableProcessors()
+http://www.inf.ufsc.br/~bosco.sobral/ensino/ine5645/Formas_de_Escalonamento_de_Threads_Java_v3.pdf
+https://www.devmedia.com.br/utilizando-threads-parte-1/4459
+*/
